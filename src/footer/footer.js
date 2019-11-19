c4071da831e4c0c8270c4d3380292090fc58ad09
Tori <victoria.j.art@gmail.com>
2019-11-19 11:04:54 -0700

    fixed footer
    and other styling

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -6,7 +6,9 @@ class Footer extends Component {
   render() {
     return (
       <footer>
+        <div id="logoftWrap">
         <small>Sarah Jain</small><img src="images/icons/logo.svg" title="Sarah Jain logo" alt="Sarah Jain leaf logo"/><span>&copy;</span>
+        </div>
         <div className="footerOval">
         </div>
         <SocialNav addClass="socialNavFt"/>
