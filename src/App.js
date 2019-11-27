ba1b36b896741f948fe7aaa04409615bacf5e4bf
Tori <victoria.j.art@gmail.com>
2019-11-27 10:41:37 -0700

    quick additon to mobile menu
    forgot to have the menu close when clicking the logo in the header
    :sweat_smile:

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
         <Router>
         <ScrollToTop />
         <header className="App-header">
-          <Link to="/"><h1>SJ</h1><img src="images/icons/logo.svg" alt ="Sarah Jain leaf logo" title="Sarah Jain logo"/></Link>
+          <Link to="/" onClick={()=> this.showMobileMenu("close")}><h1>SJ</h1><img src="images/icons/logo.svg" alt ="Sarah Jain leaf logo" title="Sarah Jain logo"/></Link>
           <div className="mobileNavBtns">
             <img onClick={()=> this.showMobileMenu("open")} src="images/icons/menu_icon.svg" id="openMenu" alt="open the navigation menu" />
             <img onClick={()=> this.showMobileMenu("close")} src="images/icons/close_icon.svg" id="closeMenu" alt="open the navigation menu" />
