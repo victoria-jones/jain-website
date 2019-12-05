6207f946f885ebf966cb2f0e33d7b90f40546eed
Tori <victoria.j.art@gmail.com>
2019-12-05 15:50:00 -0700

    reworked how modal toggles fill

---
+++ src/main/components/fillFormModal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FillForm extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>fill form tester</p>
+      </div>
+    );      
+  }
+}
+
+export default FillForm;
--- src/main/components/modal.js
+++ src/main/components/modal.js
@@ -7,9 +7,9 @@ class Modal extends Component {
 
     if(this.props.displayModal){
       if(this.props.fillModal.thankYou) {
-        this.fillThankYou();
+        //this.fillThankYou();
       } else if(this.props.fillModal.form) {
-        this.fillForm();
+        //this.fillForm();
       } else {
       }
     }
@@ -17,23 +17,60 @@ class Modal extends Component {
   }
 
   render() {
+    if(this.props.fillModal.empty) {
       return(
-
-          <div className="modalWrapper">
-            <section id="modal">
-              <img src="images/icons/close_icon.svg" className="closeBtn" alt="close window button" title="close button"
-                  onClick={() => this.props.showModal('close', 'na')}
-              />
-              <h2 id="modalHead">Modal Header</h2>
-              <section id="modalBody">
+        <div className="modalWrapper">
+          <section id="modal">
+            <img src="images/icons/close_icon.svg" className="closeBtn" alt="close window button" title="close button"
+                onClick={() => this.props.showModal('close', 'na')}
+            />
+            <h2 id="modalHead">Modal Header</h2>
+            <section id="modalBody">
+            </section>
+          </section>
+        </div>
+      );
+    } else if (this.props.fillModal.thankYou) {
+      return(
+        <div className="modalWrapper">
+          <section id="modal">
+            <img src="images/icons/close_icon.svg" className="closeBtn" alt="close window button" title="close button"
+                onClick={() => this.props.showModal('close', 'na')}
+            />
+            <h2 id="modalHead">Thank You</h2>
+            <section id="modalBody">
+              <p>Your message has been sent. I will be in touch through email soon!</p>
+              <button onClick={() => this.props.showModal('close', 'na')}>
+                ok
+              </button>
+            </section>
+          </section>
+        </div>
+      );
+    } else if (this.props.fillModal.form) {
+      return(
+        <div className="modalWrapper">
+          <section id="modal">
+            <img src="images/icons/close_icon.svg" className="closeBtn" alt="close window button" title="close button"
+                onClick={() => this.props.showModal('close', 'na')}
+            />
+            <h2 id="modalHead">Request A Booking</h2>
+            <section id="modalBody">
+              <h3>selected day goes here</h3>
+              <section id="formWrapper">
+                <form>
+                  <p>form goes here</p>
+                </form>
               </section>
             </section>
-          </div>
-
+          </section>
+        </div>
       );
+    }
+
   }
 
-  fillThankYou() {
+  /* fillThankYou() {
     let modalH2 = document.getElementById("modalHead");
     let modalBody = document.getElementById("modalBody");
 
@@ -63,7 +100,7 @@ class Modal extends Component {
     modalBody.innerHTML = "";
 
     modalH2.innerHTML = "Request a booking"
-  }
+  }*/
 }
 
 export default Modal;
